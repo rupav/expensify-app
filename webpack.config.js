21827a9a08160d907837eb2bc74453c3eb758597
rupav jain <rupavwinchester@gmail.com>
2018-04-08 13:55:54 +0530

    Added production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,85 @@
 // entry -> output, require is of node thing!
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
-// node thing :module
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader', // babel-loader uses babel-core itself
-            test: /\.js$/, // regular expression between / /, \ is escape as . is deifned in re, $ means files ending with js,
-            exclude: /(node_modules)/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader' // sass-loader uses node-sass in itself
-            ]
-        }]
-    },
-    devtool: ' cheap-module-eval-source-map ',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    console.log('env', env);
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader', // babel-loader uses babel-core itself
+                test: /\.js$/, // regular expression between / /, \ is escape as . is deifned in re, $ means files ending with js,
+                exclude: /(node_modules)/
+            }, {
+                test: /\.s?css$/,
+                // use: [
+                //     'style-loader',
+                //     'css-loader',
+                //     'sass-loader' // sass-loader uses node-sass in itself
+                // ]
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
+
+// // node thing :module
+// module.exports = {
+//     entry: './src/app.js',
+//     output: {
+//         path: path.join(__dirname, 'public'),
+//         filename: 'bundle.js'
+//     },
+//     module: {
+//         rules: [{
+//             loader: 'babel-loader', // babel-loader uses babel-core itself
+//             test: /\.js$/, // regular expression between / /, \ is escape as . is deifned in re, $ means files ending with js,
+//             exclude: /(node_modules)/
+//         }, {
+//             test: /\.s?css$/,
+//             use: [
+//                 'style-loader',
+//                 'css-loader',
+//                 'sass-loader' // sass-loader uses node-sass in itself
+//             ]
+//         }]
+//     },
+//     devtool: ' cheap-module-eval-source-map ',
+//     devServer: {
+//         contentBase: path.join(__dirname, 'public'),
+//         historyApiFallback: true
+//     }
+
+// };
 
 // loader
